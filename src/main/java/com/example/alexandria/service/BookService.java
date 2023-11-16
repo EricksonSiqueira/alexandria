6c9c1418b29609f7fbe87fd082d2db09e484ca45
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-11-16 15:12:48 -0300

    feat: create BookService

---
+++ src/main/java/com/example/alexandria/service/BookService.java
@@ -0,0 +1,55 @@
+package com.example.alexandria.service;
+
+import com.example.alexandria.models.entities.Book;
+import com.example.alexandria.models.repositories.BookRepository;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+  private BookRepository bookRepository;
+
+  @Autowired
+  public BookService(BookRepository bookRepository) {
+    this.bookRepository = bookRepository;
+  }
+
+  public Book insertBook(Book book) {
+    return bookRepository.save(book);
+  }
+
+  public Optional<Book> updateBook(Long id, Book book) {
+    Optional<Book> optionalBook = bookRepository.findById(id);
+
+    if(optionalBook.isPresent()) {
+      Book bookFromDb = optionalBook.get();
+      bookFromDb.setTitle(book.getTitle());
+      bookFromDb.setGenre(book.getGenre());
+
+      Book updatedBook = bookRepository.save(bookFromDb);
+      return Optional.of(updatedBook);
+    }
+
+    return optionalBook;
+  }
+
+  public Optional<Book> removeBookById(Long id) {
+    Optional<Book> bookOptional = bookRepository.findById(id);
+
+    if(bookOptional.isPresent()) {
+      bookRepository.deleteById(id);
+    }
+
+    return bookOptional;
+  }
+
+  public Optional<Book> getBookById(Long id) {
+    return bookRepository.findById(id);
+  }
+
+  public List<Book> getAllBooks() {
+    return bookRepository.findAll();
+  }
+}
