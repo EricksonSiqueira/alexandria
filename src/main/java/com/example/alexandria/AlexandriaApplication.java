7aaa7abb7e9d86e3cb31d80fdc702ae4ac111271
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-11-16 15:11:58 -0300

    feat:add spring to main app

--- src/main/java/com/example/alexandria/AlexandriaApplication.java
+++ src/main/java/com/example/alexandria/AlexandriaApplication.java
@@ -2,8 +2,15 @@ package com.example.alexandria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
+@EntityScan("com.example.alexandria.models.entities")
+@EnableJpaRepositories("com.example.alexandria.models.repositories")
+@ComponentScan("com.example.alexandria")
 public class AlexandriaApplication {
 
 	public static void main(String[] args) {
